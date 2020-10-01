f354b5d75c11a18dc5e5f4c9739ce5e5949b5d1e
Nitay Hurvitz <nitay.hurvitz@verint.com>
2020-10-01 19:05:16 +0300

    guy's cr comments

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 let turn = "X"
-let winner = undefined
 let turnCounter = 0;
 
 const cells = Array.from(document.getElementsByClassName("cell"))
 const board = document.getElementById("board")
-const clearBtn = document.getElementById("clear")
+const clearBtn = document.getElementById("clear-btn")
 const turnHeader = document.getElementById("turn-header")
 
 function boardClicked(event) {
@@ -12,30 +11,31 @@ function boardClicked(event) {
 
     console.log(cell.id + " clicked")
 
-    if (cell.className != "cell" || cell.innerText !== "" || !!winner) {
+    if (cell.className != "cell" || cell.innerText !== "") {
         return
     }
 
     cell.innerText = turn
     turnCounter++;
 
-    checkWinner()
+    const winner = checkWinner()
+
     if (!winner && turnCounter < 9) {
         turn = turn == "X" ? "O" : "X";
         turnHeader.innerText = `It's ${turn} turn`
     }
     else if (!!winner) {
         turnHeader.innerText = `The winner is ${winner}!`
+        board.removeEventListener("click", boardClicked)
     }
     else {
         turnHeader.innerText = `It's a tie!`
         cells.forEach(cell => cell.style.background = "green")
     }
-
-
 }
 
 function resetGame() {
+    board.addEventListener("click", boardClicked)
     cells.forEach(cell => {
         cell.innerText = ""
         cell.style.background = ""
@@ -46,6 +46,7 @@ function resetGame() {
 }
 
 function checkWinner() {
+    let winner = undefined
     const winners = [
         [0, 1, 2],
         [3, 4, 5],
@@ -58,25 +59,28 @@ function checkWinner() {
         [0, 4, 8],
         [6, 4, 2]
     ]
+    cellsText = cells.map(cell => cell.innerText)
 
     winners.forEach(cellTriplet => {
-        if (cells[cellTriplet[0]].innerText === "") {
+        if (cellsText[cellTriplet[0]] === "") {
             return
         }
-        if (cells[cellTriplet[0]].innerText ===
-            cells[cellTriplet[1]].innerText &&
-            cells[cellTriplet[1]].innerText ===
-            cells[cellTriplet[2]].innerText) {
+        if (cellsText[cellTriplet[0]] ===
+            cellsText[cellTriplet[1]] &&
+            cellsText[cellTriplet[1]] ===
+            cellsText[cellTriplet[2]]) {
 
             cells[cellTriplet[0]].style.background = "green"
             cells[cellTriplet[1]].style.background = "green"
             cells[cellTriplet[2]].style.background = "green"
-            winner = cells[cellTriplet[0]].innerText
+            winner = cellsText[cellTriplet[0]]
         }
     })
+
+    return winner
 }
 
 
-resetGame()
-board.addEventListener("click", boardClicked)
 clearBtn.addEventListener("click", resetGame)
+
+resetGame()
